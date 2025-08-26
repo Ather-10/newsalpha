fe28424378424dfaed4f5e5436a9ce0b705a5b04
Ather-10 <chatherusman05@gmail.com>
2025-08-26 22:01:24 +0500

    agiaan

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 // import PropTypes from 'prop-types'
 import React from 'react'
-import { NavLink } from 'react-router-dom';
 
 const Navbar = () => { 
 
@@ -8,22 +7,22 @@ const Navbar = () => {
                 <div>
                   <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
                   <div className="container-fluid">
-                    <a className="navbar-brand" to="/">NewsAlpha</a>
+                    <a className="navbar-brand" href="/">NewsAlpha</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                       <span className="navbar-toggler-icon"></span>
                     </button>
               <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                 <NavLink className="nav-item">
-                    <a className="nav-link active" aria-current="page" to="/">Home</a>
-                  </NavLink>
-                 <NavLink className="nav-item"> <a className="nav-link" to="business"> Business</a> </NavLink>
-                 <NavLink className="nav-item"> <a className="nav-link" to="entertainment">Entertainment</a> </NavLink>
-                 <NavLink className="nav-item"> <a className="nav-link" to="general"> General</a> </NavLink>
-                 <NavLink className="nav-item"> <a className="nav-link" to="health"> Health</a> </NavLink>
-                 <NavLink className="nav-item"> <a className="nav-link" to="science"> Science</a> </NavLink>
-                 <NavLink className="nav-item"> <a className="nav-link" to="sports"> Sports</a> </NavLink>
-                 <NavLink className="nav-item"> <a className="nav-link" to=" technology"> Technology</a> </NavLink>       
+                  <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="/">Home</a>
+                  </li>
+                  <li className="nav-item"> <a className="nav-link" href="business"> Business</a> </li>
+                  <li className="nav-item"> <a className="nav-link" href="entertainment">Entertainment</a> </li>
+                  <li className="nav-item"> <a className="nav-link" href="general"> General</a> </li>
+                  <li className="nav-item"> <a className="nav-link" href="health"> Health</a> </li>
+                  <li className="nav-item"> <a className="nav-link" href="science"> Science</a> </li>
+                  <li className="nav-item"> <a className="nav-link" href="sports"> Sports</a> </li>
+                  <li className="nav-item"> <a className="nav-link" href=" technology"> Technology</a> </li>       
                 </ul>
               </div>
             </div>
